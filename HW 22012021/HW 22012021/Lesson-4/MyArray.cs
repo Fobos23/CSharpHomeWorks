71a4c7dedf7e3e9b8d237dc8dc003f3b76ec541b
Lapshin <lapshin@skbkontur.ru>
2021-01-24 03:22:09 +0300

    Add first task

---
+++ HW 22012021/HW 22012021/Lesson-4/MyArray.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_4
+{
+    public class MyArray
+    {
+        private int[] array;
+        private int sumElements;
+        private int arrayLength;
+
+        public MyArray(int[] array)
+        {
+            this.array = array;
+            arrayLength = array.Length;
+        }
+        
+        public MyArray(int arrayLength)
+        {
+            this.arrayLength = arrayLength;
+            array = new int[arrayLength];
+        }
+
+        public int ArrayLength
+        {
+            get => arrayLength;
+        }
+
+        public int this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public void SetRandomNumbers(int minValueRange, int maxValueRange)
+        {
+            var rand = new Random();
+            for (var i = 0; i < arrayLength; i++)
+                array[i] = rand.Next(minValueRange, maxValueRange);
+        }
+
+        /// <summary>
+        /// Возвращает количество биграмм, в которых хотя бы один элемент делиться на number без остатка.
+        /// </summary>
+        /// <param name="number">Делитель для фильтрации биграмм</param>
+        /// <returns></returns>
+        public int GetBigramsCount(int number)
+        {
+            var count = 0;
+            var bigrams = GetBigramsWithWeight();
+            foreach (var bigram in bigrams)
+            {
+                if (bigram.Key % number != 0)
+                {
+                    foreach (var bigramValues in bigram.Value)
+                    {
+                        if (bigramValues.Key % number != 0)
+                            continue;
+                        count += bigramValues.Value;
+                    }
+                    continue;
+                }
+
+                foreach (var bigramValues in bigram.Value)
+                    count += bigramValues.Value;
+            }
+
+            return count;
+        }
+
+        private Dictionary<int, Dictionary<int, int>> GetBigramsWithWeight()
+        {
+            var bigrams = new Dictionary<int, Dictionary<int, int>>();
+            for (var i = 0; i < arrayLength - 1; i++)
+            {
+                if (arrayLength <= 1)
+                    throw new ArgumentException("Для работы с биграммами, массив должен иметь > 1 элемента");
+                
+                var firstPartBigram = array[i];
+                var secondPartBigram = array[i + 1];
+
+                if (!bigrams.ContainsKey(firstPartBigram))
+                    bigrams[firstPartBigram] = new Dictionary<int, int>();
+
+                if (!bigrams[firstPartBigram].ContainsKey(secondPartBigram))
+                    bigrams[firstPartBigram].Add(secondPartBigram, 1);
+                else
+                    bigrams[firstPartBigram][secondPartBigram]++;
+            }
+            return bigrams;
+        }
+    }
+}
---
+++ HW 22012021/HW 22012021/Lesson-4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lesson_4
+{
+    class Program
+    {
+        // Выполнил Андрей Лапшин
+        
+        static void Main(string[] args)
+        {
+            var number = 0;
+            Console.Write("Введи номер задачи от 1 до 4: ");
+            if (!int.TryParse(Console.ReadLine(), out number) || number > 4)
+                throw new ArgumentException("Вы ввели некорректное число!");
+
+            switch(number)
+            {
+                case 1:
+                    StartTask1();
+                    break;
+            }
+        }
+
+        private static void StartTask1()
+        {
+            /* Дан целочисленный массив из 20 элементов. Элементы массива могут принимать целые значения
+               от –10 000 до 10 000 включительно. Написать программу, позволяющую найти и вывести количество
+               пар элементов массива, в которых хотя бы одно число делится на 3. В данной задаче под парой
+               подразумевается два подряд идущих элемента массива. Например, для массива из
+               пяти элементов: 6; 2; 9; –3; 6 – ответ: 4.*/
+            
+            var myArray = new MyArray(20);
+            
+            myArray.SetRandomNumbers(-10000, 10000);
+            for (var i = 0; i < myArray.ArrayLength; i++)
+                Console.WriteLine($"{myArray[i]} ");
+
+            var denominator = 3;
+            var bigramsCount = myArray.GetBigramsCount(denominator);
+            Console.WriteLine($"\nКоличетсво биграмм, в которых хотя бы 1 число делиться на {denominator} без остатка, равно {bigramsCount}");
+        }
+    }
+}
