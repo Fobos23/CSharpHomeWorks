76eed04d2dfb715d3cb88ec10549afece1b90869
Lapshin <lapshin@skbkontur.ru>
2021-01-27 00:09:49 +0300

    Добавил первое задание

---
+++ HW 26012021/HW 26012021/Lesson-5/Auth.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Lesson_5
+{
+    public class Auth
+    {
+        public bool SignUp(string login, string pass)
+        {
+            return IsCorrectLogin(login);
+        }
+        
+        /// <summary>
+        /// Тестовая перегрузка, для вызова метода проверки логина без регулярок
+        /// </summary>
+        /// <param name="login"></param>
+        /// <returns></returns>
+        public bool SignUp(string login)
+        {
+            return IsCorrectlyLogin(login);
+        }
+
+        private bool IsCorrectLogin(string login)
+        {
+            var pattern = @"^[^0-9][A-Za-z0-9]{1,9}$";
+            var rgx = new Regex(pattern);
+            return rgx.IsMatch(login);
+        }
+
+        private bool IsCorrectlyLogin(string login)
+        {
+            if (char.IsDigit(login[0]) 
+                || login.Length < 2 || login.Length > 10)
+                return false;
+
+            foreach (var sign in login)
+            {
+                if (!(char.IsDigit(sign) 
+                    || sign >= 'a' && sign <= 'z' 
+                    || sign >= 'A' && sign <= 'Z'))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ HW 26012021/HW 26012021/Lesson-5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        // Выполнил Андрей Лапшин
+
+        static void Main(string[] args)
+        {
+            var number = 0;
+            Console.Write("Введи номер задачи от 1 до 4: ");
+            if (!int.TryParse(Console.ReadLine(), out number) || number > 5)
+                throw new ArgumentException("Вы ввели некорректное число!");
+
+            switch(number)
+            {
+                case 1:
+                    StartTask1();
+                    break;
+            }
+        }
+
+        private static void StartTask1()
+        { 
+            /* 1. Создать программу, которая будет проверять корректность ввода логина. Корректным логином
+               будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при 
+               этом цифра не может быть первой:
+               а) без использования регулярных выражений;
+               б) с использованием регулярных выражений.*/
+
+            var auth = new Auth();
+            while (true)
+            {
+                Console.WriteLine("Проверка логина регуляркой!");
+                Console.Write("Логин: ");
+                var login = Console.ReadLine();
+                if (auth.SignUp(login, "1234"))
+                {
+                   Console.WriteLine("Пользователь создан!");
+                   break;
+                }
+                Console.WriteLine("Логин некорректный, придумайте новый");
+            }
+            
+            while (true)
+            {
+                Console.WriteLine("Проверка логина без регулярок!");
+                Console.Write("Логин: ");
+                var login = Console.ReadLine();
+                if (auth.SignUp(login))
+                {
+                    Console.WriteLine("Пользователь создан!");
+                    break;
+                }
+                Console.WriteLine("Логин некорректный, придумайте новый");
+            }
+            
+        }
+    }
+}
